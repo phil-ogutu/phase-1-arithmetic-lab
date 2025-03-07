75c8a3ec8d555c9278f34701085425228f3e9d7c
phil-ogutu <phil.shifwoko@gmail.com>
2025-03-07 17:53:37 +0300

    Arithmetic Operations

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.floor((Math.random() * 10) + 1)
+
+let num3 = 119
+let num4 = 5
+
+let mod = num3 % num4
+
+let max = Math.max(12,13,6,8,20)
